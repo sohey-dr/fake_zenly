8b3d4524046363c50f56a5b065d00689bd0a2cf7
soheyHashimoto <heyso1209@gmail.com>
2021-08-15 18:00:14 +0900

    Initial Commit

---
+++ fake_zenly/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fake_zenly
+//
+//  Created by 橋本創平 on 2021/08/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fake_zenly/fake_zenlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  fake_zenlyApp.swift
+//  fake_zenly
+//
+//  Created by 橋本創平 on 2021/08/15.
+//
+
+import SwiftUI
+
+@main
+struct fake_zenlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
